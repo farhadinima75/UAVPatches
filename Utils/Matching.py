a1e48e41c6eb40882772e2b49214621630a4737c
farhadinima75 <farhadinima75@email.kntu.ac.ir>
2022-03-04 17:21:41 +0330

    Update Matching.py

--- Utils/Matching.py
+++ Utils/Matching.py
@@ -5,7 +5,8 @@ __all__ = ['LocalFeatureExtractor', 'DescriptorMatcher', 'GeometricVerifier', 'S
            'SIFT_SIFT', 'SIFT_HARDNET','SIFT_SOSNET', 'SIFT_L2NET','SIFT_ROOT_SIFT', 'SIFT_GEODESC', 'SIFT_TFEAT', 'SIFT_BROWN6',
            'SIFT_UAVPatches', 'SIFT_UAVPatchesPlus','CONTEXTDESC_CONTEXTDESC', 'D2NET_D2NET','R2D2_R2D2', 'KEYNET_KEYNET', 'ORB2_ORB2',
            'AKAZE_AKAZE', 'SUPERPOINT_SUPERPOINT', 'LFNET_LFNET', 'SIFT_LOGPOLAR', 'MSER_HARDNET', 'DISK_DISK', 'DELF_DELF', 'SIFT_VGG',
-           'SIFT_DAISY', 'SIFT_BOOST_DESC', 'SIFT_LATCH', 'SIFT_FREAK', 'ORB2_UAVPatchesPlus', 'ORB2_UAVPatches', 'ORB2_BROWN6', 'ORB2_HARDNET']
+           'SIFT_DAISY', 'SIFT_BOOST_DESC', 'SIFT_LATCH', 'SIFT_FREAK', 'ORB2_UAVPatchesPlus', 'ORB2_UAVPatches', 'ORB2_BROWN6', 'ORB2_HARDNET',
+           'ORB2_SOSNET']
 
 import cv2
 import numpy as np
@@ -222,7 +223,7 @@ def SIFT_UAVPatches(num_features):
                    descriptor_type = FeatureDescriptorTypes.UAVPatches)  
     Feature = FeatureManagerConfigs.extract_from(Feature)
     Feature = feature_manager_factory(**Feature)
-    Feature._feature_descriptor.mag_factor = 12
+    Feature._feature_descriptor.mag_factor = 18
     return Feature 
 
 def SIFT_UAVPatchesPlus(num_features):
@@ -231,7 +232,7 @@ def SIFT_UAVPatchesPlus(num_features):
                    descriptor_type = FeatureDescriptorTypes.UAVPatchesPlus)  
     Feature = FeatureManagerConfigs.extract_from(Feature)
     Feature = feature_manager_factory(**Feature)
-    Feature._feature_descriptor.mag_factor = 12
+    Feature._feature_descriptor.mag_factor = 18
     return Feature     
 
 def SIFT_BROWN6(num_features):
@@ -327,8 +328,6 @@ def DISK_DISK(num_features):
                    descriptor_type = FeatureDescriptorTypes.DISK)  
     Feature = FeatureManagerConfigs.extract_from(Feature)
     Feature = feature_manager_factory(**Feature) 
-    Feature._feature_detector.OutWidth = 16*400
-    Feature._feature_detector.OutHeight = 16*200
     return Feature
 
 def MSER_HARDNET(num_features):
@@ -410,6 +409,17 @@ def ORB2_BROWN6(num_features):
     Feature._feature_descriptor.mag_factor = 1
     return Feature
 
+def ORB2_SOSNET(num_features):
+    Feature = dict(num_features    = num_features,
+                   num_levels = 8, 
+                   scale_factor = 1.2,   
+                   detector_type   = FeatureDetectorTypes.ORB2, 
+                   descriptor_type = FeatureDescriptorTypes.SOSNET)  
+    Feature = FeatureManagerConfigs.extract_from(Feature)
+    Feature = feature_manager_factory(**Feature) 
+    Feature._feature_descriptor.mag_factor = 1
+    return Feature
+
 # Cell
 class SNNMatcher():
     def __init__(self, th = 0.8):
@@ -438,7 +448,7 @@ class SNNMMatcher():
         else:
             dev = torch.device('cpu')
 
-        dists, idxs = match_smnn(torch.from_numpy(queryDescriptors).float().to(dev),
+        dists, idxs = match_snn(torch.from_numpy(queryDescriptors).float().to(dev),
                                    torch.from_numpy(trainDescriptors).float().to(dev),
                                    self.th)
         good_matches = []
@@ -503,14 +513,14 @@ class TwoViewMatcher():
         good_kpts2 = [ kps2[m.trainIdx] for i,m in enumerate(tentative_matches) if mask[i]]
 
         # good_kpts1pt, good_kpts2pt = np.float32([K.pt for K in good_kpts1]), np.float32([K.pt for K in good_kpts2])
-        # H, maskH = self.geom_verif.verify(good_kpts1pt, good_kpts2pt, H=True)
+        # H, maskH = self.geom_verif.verify(good_kpts2pt, good_kpts1pt, H=True)
         ReprojectionError = compute_hom_reprojection_error(H, np.float32([K.pt for K in good_kpts2]), 
                                                               np.float32([K.pt for K in good_kpts1]))
         print(f'\033[92m3 x sigma-MAD of descriptor distances: {3*SigmaMad:.4f}\033[0m')
         print(f'\033[92mHemographic reprojection error: {ReprojectionError:.4f}\033[0m')
         print(f'\033[92mFinal Matches: {len(good_kpts1)}\033[0m')
         print(f'\033[92mInliers Ratio: {float(len(good_kpts1))/float(len(src_pts)):.4f}\033[0m')
-        print(f'\033[92mDescriptor Time: {T2 - T1:.2f}\033[0m')
+        print(f'\033[92mDetector and Descriptor Time: {T2 - T1:.2f}\033[0m')
         
         result = {'img1': img1, 'img2': img2,
                   'init_kpts1': kps1,
